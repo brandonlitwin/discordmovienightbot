2c8265d377831f8be4250ea937dcf5c969bae459
Brandon Litwin <brandon.litwin1@marist.edu>
2020-07-16 16:58:37 -0400

    Add indexes to poll

--- poll.py
+++ poll.py
@@ -5,20 +5,28 @@ import random
 
 def create_poll(num_minutes):
     unviewed_movies = []
-    for post in config.collection.find({"viewed": False}):
-        current_movie_str = str(post['title']) + \
-         " (https://imdb.com/title/" + str(post['imdbID']) + \
-         "), submitted by @" + str(post['submitter']) + "\n"
+    all_movies = config.collection.find({"viewed": False})
+
+    for movie in config.collection.find({"viewed": False}):
+        current_movie_str = str(movie['title']) + \
+         " (https://imdb.com/title/" + str(movie['imdbID']) + \
+         "), submitted by @" + str(movie['submitter']) + "\n"
         unviewed_movies.append(current_movie_str)
 
     poll_list = unviewed_movies
 
+    poll_list_with_indexes = []
+
     if len(unviewed_movies) > 10:
         poll_list = random.sample(unviewed_movies, k=10)
 
+    for index, movie in enumerate(poll_list, start=1):
+        current_movie_str = str(index) + " " + movie
+        poll_list_with_indexes.append(current_movie_str)
+
     poll_list_str = ""
 
-    for movie in poll_list:
+    for movie in poll_list_with_indexes:
         poll_list_str += movie + "\n"
 
     return poll_list_str
