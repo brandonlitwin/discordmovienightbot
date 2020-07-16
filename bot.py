1c4ebd232e2f06d80fed5caec8b666b972c27c9c
Brandon Litwin <brandon.litwin1@marist.edu>
2020-07-16 14:32:33 -0400

    styling

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
-from update_list import add_movie, check_movie_in_list, check_movie_in_any_list, remove_movie
+from update_list import add_movie, check_movie_in_list
+from update_list import check_movie_in_any_list, remove_movie
 from show_list import show_list
 from set_viewed import set_viewed
 from poll import create_poll
@@ -13,8 +14,10 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='--')
 
-@bot.command(name='poll', help='Select 10 random movies and create a poll. Default time 60 min, max 1440')
-async def poll(ctx, num_minutes: int=60):
+
+@bot.command(name='poll', help='Select 10 random movies and create a poll.' +
+             'Default time 60 min, max 1440')
+async def poll(ctx, num_minutes: int = 60):
     if num_minutes > 1440:
         num_minutes = 1440
     if num_minutes == 1:
@@ -29,6 +32,7 @@ async def poll(ctx, num_minutes: int=60):
 
     await ctx.send("```" + response + "```")
 
+
 @bot.command(name='add', help='Add movie to the watch list. IMDB link only.')
 async def add(ctx, link: str):
     if "imdb.com" in link:
@@ -42,13 +46,17 @@ async def add(ctx, link: str):
         response = "Please provide valid IMDB link."
     await ctx.send(response)
 
+
 @bot.command(name='list', help='Current unwatched movies.')
 async def list(ctx):
     response = show_list(viewed=False)
     movie_list = ""
     for movie in response:
-        movie_list = movie_list + movie['title'] + " (" + movie['year'] + "), submitted by @" + movie['submitter'] + "\n"
+        movie_list = movie_list + \
+            movie['title'] + " (" + movie['year'] + \
+            "), submitted by @" + movie['submitter'] + "\n"
     await ctx.send("```" + "Unviewed Movies \n" + movie_list + "```")
+
 
 @bot.command(name='viewedlist', help='Current watched movies.')
 async def viewedlist(ctx):
@@ -56,11 +64,15 @@ async def viewedlist(ctx):
     print(response)
     movie_list = ""
     for movie in response:
-        movie_list = movie_list + movie['title'] + " (" + movie['year'] + "), submitted by @" + movie['submitter'] + ", viewed on " + str(movie['viewedDate']).split(" ")[0] + "\n"
+        movie_list = movie_list + movie['title'] + " (" + movie['year'] + \
+            "), submitted by @" + \
+            movie['submitter'] + ", viewed on " + \
+            str(movie['viewedDate']).split(" ")[0] + "\n"
     if movie_list == "":
         await ctx.send("No movies have been viewed yet.")
     else:
         await ctx.send("```" + "Viewed Movies \n" + movie_list + "```")
+
 
 @bot.command(name='setviewed', help='Put movie in viewed list. IMDB link only')
 async def setviewed(ctx, link):
@@ -76,6 +88,7 @@ async def setviewed(ctx, link):
     else:
         response = "Please provide valid IMDB link."
     await ctx.send(response)
+
 
 @bot.command(name='remove', help='Remove from watch list. IMDB link only.')
 async def remove(ctx, link: str):
