aa60acd7473806392fd33e1e843066e7d6387fc1
Brandon Litwin <brandon.litwin1@marist.edu>
2020-07-20 17:19:55 -0400

    add global dict to track poll votes

--- poll.py
+++ poll.py
@@ -9,8 +9,8 @@ def create_poll(num_minutes):
 
     for movie in config.collection.find({"viewed": False}):
         current_movie_str = str(movie['title']) + \
-         " (https://imdb.com/title/" + str(movie['imdbID']) + \
-         "), submitted by @" + str(movie['submitter']) + "\n"
+            " (https://imdb.com/title/" + str(movie['imdbID']) + \
+            "), submitted by @" + str(movie['submitter']) + "\n"
         unviewed_movies.append(current_movie_str)
 
     poll_list = unviewed_movies
@@ -30,3 +30,15 @@ def create_poll(num_minutes):
         poll_list_str += movie + "\n"
 
     return poll_list_str
+
+
+def poll_to_dict(poll_str):
+    poll_dict = {}
+    movie_list = list(filter(bool, poll_str.splitlines()))
+    for movie in movie_list:
+        poll_dict[str(movie.split(')')[0])] = {'title':
+                                               movie.split('(')[0].split(')')[
+                                                   1].strip(),
+                                               'votes': 0}
+
+    return poll_dict
