87a9404ac36f12221a2cee8dc34ac507251aa5e7
Brandon Litwin <brandon.litwin1@marist.edu>
2020-08-28 12:17:44 -0400

    Readme update

--- config.py
+++ config.py
@@ -5,16 +5,11 @@ import pymongo
 from pymongo import MongoClient
 
 load_dotenv()
-REQUEST_URL = os.getenv('OMDB_REQUEST_URL')
 API_KEY = os.getenv('OMDB_API_KEY')
 TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL = os.getenv('CHANNEL_ID')
 SERVER = os.getenv('SERVER_ID')
 
-# USERNAME = urllib.parse.quote_plus(os.getenv('CONNECTION_USER'))
-# PASSWORD = urllib.parse.quote_plus(os.getenv('CONNECTION_PASSWORD'))
-
-# CONNECTION_STRING = f'mongodb+srv://{USERNAME}:{PASSWORD}@movienightbot-dttqs.mongodb.net/test'
 CONNECTION_STRING = 'mongodb://localhost:27017/'
 
 cluster = MongoClient(CONNECTION_STRING)
